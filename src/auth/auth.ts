929a1214dd86d44608cd20312da640508b9f3d09
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-06-30 16:17:17 -0300

    rename firebase app

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,5 @@
 import { initializeAuth } from "firebase/auth";
-import { firebaseApp } from "../services/firebaseConfig";
+import { firebaseApp } from "../services/firebaseApp";
 import { persistence } from "./persistence";
 
 export const auth = initializeAuth(firebaseApp, {
