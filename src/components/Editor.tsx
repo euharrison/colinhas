f07f31f6aacd6503fad03efc3fdee8340bc993cc
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-06-29 23:50:11 -0300

    update keyboard

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
 import { Sheet } from "../database/types";
-import { KeyboardLayout } from "./KeyboardLayout";
 import { NotesKeyboard } from "./NotesKeyboard";
 import { SaveModal } from "./SaveModal";
 
@@ -17,51 +16,47 @@ export const Editor = ({ sheet }: { sheet?: Sheet }) => {
 
   return (
     <>
-      <KeyboardLayout>
-        <View style={{ flex: 1 }}>
-          <TextInput
-            ref={inputRef}
-            style={{
-              marginHorizontal: 20,
-              flex: 1,
-              fontSize: 20,
-            }}
-            placeholder="Do Re Mi Fa..."
-            multiline
-            autoCapitalize="none"
-            autoComplete="off"
-            autoCorrect={false}
-            autoFocus
-            textAlignVertical="top"
-            value={value}
-            onChangeText={(newValue) => {
-              setValue(newValue);
-            }}
-          />
-          <Pressable
-            style={({ pressed }) => ({
-              height: 50,
-              width: 50,
-              borderRadius: 50,
-              borderWidth: 1,
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: pressed ? "#ccc" : "#ffffffaa",
-              position: "absolute",
-              right: 20,
-              bottom: 0,
-            })}
-            onPress={() => {
-              setSaveModalVisible(true);
-            }}
-          >
-            <Text>{">"}</Text>
-          </Pressable>
-        </View>
-        <View style={{ paddingHorizontal: 8, paddingVertical: 20 }}>
-          <NotesKeyboard onPress={onPressNoteKeyboard} />
-        </View>
-      </KeyboardLayout>
+      <View style={{ flex: 1 }}>
+        <TextInput
+          ref={inputRef}
+          style={{
+            marginHorizontal: 20,
+            flex: 1,
+            fontSize: 20,
+          }}
+          placeholder="Do Re Mi Fa..."
+          multiline
+          autoCapitalize="none"
+          autoComplete="off"
+          autoCorrect={false}
+          autoFocus
+          textAlignVertical="top"
+          value={value}
+          onChangeText={(newValue) => {
+            setValue(newValue);
+          }}
+        />
+        <Pressable
+          style={({ pressed }) => ({
+            height: 50,
+            width: 50,
+            borderRadius: 50,
+            borderWidth: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: pressed ? "#ccc" : "#ffffffaa",
+            position: "absolute",
+            right: 8,
+            bottom: 8,
+          })}
+          onPress={() => {
+            setSaveModalVisible(true);
+          }}
+        >
+          <Text>{">"}</Text>
+        </Pressable>
+      </View>
+      <NotesKeyboard onPress={onPressNoteKeyboard} />
       <SaveModal
         sheet={sheet}
         data={value}
--- src/components/NotesKeyboard.tsx
+++ src/components/NotesKeyboard.tsx
@@ -1,48 +1,11 @@
 import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
-
-const Key = ({
-  children,
-  onPress,
-}: {
-  children: string;
-  onPress: (value: string) => void;
-}) => {
-  return (
-    <Pressable
-      onPress={() => onPress(children)}
-      style={({ pressed }) => ({
-        flexGrow: 1,
-        flexBasis: 1,
-        borderWidth: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 2,
-        backgroundColor: pressed ? "#ccc" : undefined,
-      })}
-    >
-      <Text selectable={false}>{children}</Text>
-    </Pressable>
-  );
-};
-
-const NotesRow = ({
-  notes,
-  onPress,
-}: {
-  notes: string[];
-  onPress: (value: string) => void;
-}) => {
-  return (
-    <View style={{ flexDirection: "row", gap: 1, height: 32 }}>
-      {notes.map((key) => (
-        <Key onPress={() => onPress(`${key} `)} key={key}>
-          {key}
-        </Key>
-      ))}
-    </View>
-  );
-};
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 
 const scales = [
   ["do♭", "re♭", "mi♭", "fa♭", "sol♭", "la♭", "si♭"],
@@ -62,6 +25,48 @@ const scales = [
   ["do#", "re#", "mi#", "fa#", "sol#", "la#", "si#"],
 ];
 
+const Key = ({
+  children,
+  onPress,
+}: {
+  children: string;
+  onPress: (value: string) => void;
+}) => {
+  return (
+    <Pressable
+      onPress={() => onPress(children)}
+      style={({ pressed }) => ({
+        flexGrow: 1,
+        flexBasis: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 4,
+        backgroundColor: pressed ? "#ccc" : "white",
+      })}
+    >
+      <Text selectable={false}>{children}</Text>
+    </Pressable>
+  );
+};
+
+const NotesRow = ({
+  notes,
+  onPress,
+}: {
+  notes: string[];
+  onPress: (value: string) => void;
+}) => {
+  return (
+    <View style={{ flexDirection: "row", gap: 2, height: 32 }}>
+      {notes.map((key) => (
+        <Key key={key} onPress={() => onPress(`${key} `)}>
+          {key}
+        </Key>
+      ))}
+    </View>
+  );
+};
+
 export const NotesKeyboard = ({
   onPress,
 }: {
@@ -72,31 +77,42 @@ export const NotesKeyboard = ({
   const keys = scales[scaleIndex];
 
   return (
-    <View style={{ marginBottom: 8, gap: 1 }}>
-      <View style={{ flexDirection: "row", gap: 1, height: 28 }}>
-        <Key
-          onPress={() => {
-            setScaleIndex((s) => Math.max(s - 1, 0));
-            onPress("");
-          }}
-        >
-          ↓ ♭
-        </Key>
-        <Key
-          onPress={() => {
-            setScaleIndex((s) => Math.min(s + 1, scales.length - 1));
-            onPress("");
-          }}
-        >
-          ↑ #
-        </Key>
+    <KeyboardAvoidingView behavior="padding">
+      <View
+        style={{
+          padding: 8,
+          paddingBottom: Platform.OS === "ios" ? 28 : 8,
+          gap: 2,
+          backgroundColor: "#ddd",
+        }}
+      >
+        <View style={{ flexDirection: "row", gap: 2, height: 28 }}>
+          <Key onPress={() => onPress("♭ ")}>♭</Key>
+          <Key
+            onPress={() => {
+              setScaleIndex((s) => Math.max(s - 1, 0));
+              onPress("");
+            }}
+          >
+            ↓ ♭
+          </Key>
+          <Key
+            onPress={() => {
+              setScaleIndex((s) => Math.min(s + 1, scales.length - 1));
+              onPress("");
+            }}
+          >
+            ↑ #
+          </Key>
+          <Key onPress={() => onPress("# ")}>#</Key>
+        </View>
+        <NotesRow notes={keys.map((s) => s.toUpperCase())} onPress={onPress} />
+        <NotesRow
+          notes={keys.map((s) => s[0].toUpperCase() + s.slice(1))}
+          onPress={onPress}
+        />
+        <NotesRow notes={keys} onPress={onPress} />
       </View>
-      <NotesRow notes={keys.map((s) => s.toUpperCase())} onPress={onPress} />
-      <NotesRow
-        notes={keys.map((s) => s[0].toUpperCase() + s.slice(1))}
-        onPress={onPress}
-      />
-      <NotesRow notes={keys} onPress={onPress} />
-    </View>
+    </KeyboardAvoidingView>
   );
 };
