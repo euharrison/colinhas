36b451a1e8ddb9e21b8c090e320ca6974b19e1d9
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-05 17:35:01 -0300

    add google redirect login

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,19 +1,64 @@
 import { router } from "expo-router";
-import { View } from "react-native";
+import { AuthError } from "firebase/auth";
+import { Alert, Pressable, ScrollView, Text, View } from "react-native";
+import { googleLogin } from "../auth/googleLogin";
+import { logout } from "../auth/logout";
 import { Header } from "../components/Header";
 import { InstrumentSelector } from "../components/InstrumentSelector";
+import { useUser } from "../hooks/useUser";
 
 export const SettingsPage = () => {
+  const user = useUser();
+
   return (
     <>
       <Header title="Configurações" />
-      <View style={{ padding: 20 }}>
+      <ScrollView
+        contentContainerStyle={{
+          padding: 20,
+          paddingBottom: 100,
+          gap: 40,
+        }}
+      >
         <InstrumentSelector
           onChange={() => {
             router.back();
           }}
         />
-      </View>
+        {user ? (
+          <View>
+            <Text>{user.displayName}</Text>
+            <Text>{user.email}</Text>
+            <Pressable
+              style={{ alignSelf: "flex-start", marginTop: 10 }}
+              onPress={async () => {
+                try {
+                  await logout();
+                } catch (error) {
+                  Alert.alert(String(error));
+                }
+              }}
+            >
+              <Text>(Sair)</Text>
+            </Pressable>
+          </View>
+        ) : (
+          <Pressable
+            style={{ alignSelf: "flex-start" }}
+            onPress={async () => {
+              try {
+                await googleLogin();
+              } catch (error) {
+                if ((error as AuthError).code !== "auth/popup-closed-by-user") {
+                  Alert.alert(String(error));
+                }
+              }
+            }}
+          >
+            <Text>Entrar com Google</Text>
+          </Pressable>
+        )}
+      </ScrollView>
     </>
   );
 };
