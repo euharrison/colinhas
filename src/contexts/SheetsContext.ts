014fc2500f4ca594e3dd56433fee8704c859d71d
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-07 15:52:38 -0300

    delete sheets context

--- src/contexts/SheetsContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { Sheet } from "../database/types";
-
-export const SheetsContext = createContext<Sheet[]>([]);
--- src/hooks/useSheetList.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { SheetsContext } from "../contexts/SheetsContext";
-
-export const useSheetList = () => {
-  const data = useContext(SheetsContext);
-  return data.sort((a, b) => b.updatedAt - a.updatedAt);
-};
