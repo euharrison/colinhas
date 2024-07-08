36050ce49e81e831c344c5a34a8affc184536a9d
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-08 01:01:44 -0300

    move sheet db functions

--- src/database/deleteSheet.ts
+++
@@ -1,6 +0,0 @@
-import { deleteDoc, doc } from "firebase/firestore";
-import { db, sheetsCollection } from "./db";
-
-export async function deleteSheet(id: string) {
-  return await deleteDoc(doc(db, sheetsCollection, id));
-}
