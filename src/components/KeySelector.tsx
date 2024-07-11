176068ae748778d0928193590d894bed68c114e1
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-10 23:26:56 -0300

    invert key selector options

--- src/components/KeySelector.tsx
+++ src/components/KeySelector.tsx
@@ -20,20 +20,21 @@ export const KeySelector = ({
       onChange={(e) => onChange(e.currentTarget.value)}
     >
       {[
-        { value: Key["Do#"], label: "Do♯ - ♯♯♯♯♯♯♯" },
-        { value: Key["Fa#"], label: "Fa♯ - ♯♯♯♯♯♯" },
-        { value: Key.Si, label: "Si - ♯♯♯♯♯" },
-        { value: Key.Mi, label: "Mi - ♯♯♯♯" },
-        { value: Key.La, label: "La - ♯♯♯" },
-        { value: Key.Re, label: "Re - ♯♯" },
-        { value: Key.Sol, label: "Sol - ♯" },
-        { value: Key.Do, label: "Do" },
-        { value: Key.Fa, label: "Fa - ♭" },
-        { value: Key.Sib, label: "Si♭ - ♭♭" },
-        { value: Key.Mib, label: "Mi♭ - ♭♭♭" },
-        { value: Key.Lab, label: "La♭ - ♭♭♭♭" },
-        { value: Key.Reb, label: "Re♭ - ♭♭♭♭♭" },
+        { value: Key.Dob, label: "Do♭ - ♭♭♭♭♭♭♭" },
         { value: Key.Solb, label: "Sol♭ - ♭♭♭♭♭♭" },
+        { value: Key.Reb, label: "Re♭ - ♭♭♭♭♭" },
+        { value: Key.Lab, label: "La♭ - ♭♭♭♭" },
+        { value: Key.Mib, label: "Mi♭ - ♭♭♭" },
+        { value: Key.Sib, label: "Si♭ - ♭♭" },
+        { value: Key.Fa, label: "Fa - ♭" },
+        { value: Key.Do, label: "Do" },
+        { value: Key.Sol, label: "Sol - ♯" },
+        { value: Key.Re, label: "Re - ♯♯" },
+        { value: Key.La, label: "La - ♯♯♯" },
+        { value: Key.Mi, label: "Mi - ♯♯♯♯" },
+        { value: Key.Si, label: "Si - ♯♯♯♯♯" },
+        { value: Key["Fa#"], label: "Fa♯ - ♯♯♯♯♯♯" },
+        { value: Key["Do#"], label: "Do♯ - ♯♯♯♯♯♯♯" },
       ]
         .filter(nonNullable)
         .map(({ value, label }) => (
