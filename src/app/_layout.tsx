3910489d049f1eb7464cf655981aeca7a572e94e
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-11 01:42:44 -0300

    remove outline

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,13 @@
 import { Stack } from "expo-router/stack";
+import { Platform } from "react-native";
 import { AccidentalProvider } from "../components/AccidentalProvider";
 import { AuthProvider } from "../components/AuthProvider";
 import { InstrumentProvider } from "../components/InstrumentProvider";
 import { white } from "../theme/colors";
+
+if (Platform.OS === "web") {
+  require("../theme/globalWebStyles.css");
+}
 
 export default function Layout() {
   return (
