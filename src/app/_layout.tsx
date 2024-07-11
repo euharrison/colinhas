50984fbd6c8568a12da9566409f156888fbec4bc
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-11 15:15:18 -0300

    improve support for pwa

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,8 @@
 import { Stack } from "expo-router/stack";
-import { Platform } from "react-native";
 import { AuthProvider } from "../components/AuthProvider";
 import { LocalSettingsProvider } from "../components/LocalSettingsProvider";
 import { OnboardingGate } from "../components/OnboardingGate";
 import { white } from "../theme/colors";
-
-if (Platform.OS === "web") {
-  require("../theme/globalWebStyles.css");
-}
 
 export default function Layout() {
   return (
