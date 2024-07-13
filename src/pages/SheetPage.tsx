8f0919627387eb69f46a97d3748b4a43b2f7e051
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-13 20:21:54 -0300

    add share dialog

--- src/pages/SheetPage.tsx
+++ src/pages/SheetPage.tsx
@@ -1,31 +1,28 @@
-import { useLocalSearchParams } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Platform, Pressable, Share, Text, View } from "react-native";
-import { auth } from "../auth/auth";
+import { Pressable, Text, View } from "react-native";
+import { Button } from "../components/Button";
+import { Dialog } from "../components/Dialog";
 import { EditSheet } from "../components/EditSheet";
 import { Header } from "../components/Header";
 import { KeyboardLayout } from "../components/KeyboardLayout";
 import { LoadingPage } from "../components/LoadingPage";
+import { SheetMenu } from "../components/SheetMenu";
 import { ViewSheet } from "../components/ViewSheet";
-import { deleteSheet, observeSheet } from "../database/sheet";
+import { observeSheet } from "../database/sheet";
 import { Sheet } from "../database/types";
 import { OptionsIcon } from "../icons/OptionsIcons";
-import { PencilIcon } from "../icons/PencilIcon";
-import { ResetIcon } from "../icons/ResetIcon";
-import { ShareIcon } from "../icons/ShareIcon";
-import { TrashIcon } from "../icons/TrashIcon";
+import { WhatsappIcon } from "../icons/WhatsappIcon";
 import { alert } from "../services/alert";
-import { dismissAll } from "../services/navigation";
-import { backgroundGray, black, white } from "../theme/colors";
-import { dropShadow } from "../theme/shadows";
+import { textGray } from "../theme/colors";
 import { headerHeight, pagePadding } from "../theme/sizes";
 import { shareSheetUrl } from "../urls";
-import { nonNullable } from "../utils";
 import { NotFoundPage } from "./NotFoundPage";
 
 export const SheetPage = () => {
   const [isEditMode, setIsEditMode] = useState(false);
-  const [isOptionsVisible, setIsOptionVisible] = useState(false);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [isShareVisible, setIsShareVisible] = useState(false);
 
   const [sheet, setSheet] = useState<Sheet | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(true);
@@ -51,117 +48,70 @@ export const SheetPage = () => {
     return <NotFoundPage />;
   }
 
-  const showShareButton = Platform.OS !== "web";
-  const showOptionsMenu =
-    showShareButton || sheet.userId === auth.currentUser?.uid;
-
   return (
     <KeyboardLayout>
       <Header title={sheet.name}>
-        {showOptionsMenu && (
-          <Pressable
-            style={{
-              height: headerHeight,
-              paddingHorizontal: pagePadding,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            onPress={() => setIsOptionVisible((v) => !v)}
-          >
-            <OptionsIcon />
-          </Pressable>
-        )}
+        <Pressable
+          style={{
+            height: headerHeight,
+            paddingHorizontal: pagePadding,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => setIsMenuVisible((v) => !v)}
+        >
+          <OptionsIcon />
+        </Pressable>
       </Header>
 
       {isEditMode ? <EditSheet sheet={sheet} /> : <ViewSheet sheet={sheet} />}
 
-      {isOptionsVisible && (
-        <>
-          <Pressable
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-            onPress={() => setIsOptionVisible(false)}
-          />
-          <View
-            style={{
-              position: "absolute",
-              right: 0,
-              top: headerHeight,
-              width: 200,
-              borderWidth: 1,
-              borderColor: black,
-              borderRadius: 8,
-              paddingVertical: 8,
-              backgroundColor: white,
-              ...dropShadow,
+      <SheetMenu
+        sheet={sheet}
+        isVisible={isMenuVisible}
+        setIsVisible={setIsMenuVisible}
+        isEditMode={isEditMode}
+        setIsEditMode={setIsEditMode}
+        setIsShareVisible={setIsShareVisible}
+      />
+
+      <Dialog
+        title="Share"
+        visible={isShareVisible}
+        onRequestClose={() => setIsShareVisible(false)}
+      >
+        <Text
+          selectable
+          numberOfLines={1}
+          style={{ color: textGray, marginBottom: 16 }}
+        >
+          {shareSheetUrl(sheet)}
+        </Text>
+        <View style={{ flexDirection: "row", alignItems: "stretch", gap: 8 }}>
+          <Button
+            style={{ flex: 1 }}
+            onPress={async () => {
+              try {
+                await navigator.clipboard.writeText(shareSheetUrl(sheet));
+                alert("Link copiado");
+                setIsShareVisible(false);
+              } catch (error) {
+                alert(String(error));
+              }
             }}
           >
-            {[
-              showShareButton
-                ? {
-                    label: "Compartilhar",
-                    icon: <ShareIcon width={18} />,
-                    onPress: () => Share.share({ url: shareSheetUrl(sheet) }),
-                  }
-                : undefined,
-              isEditMode
-                ? {
-                    label: "Cancelar edição",
-                    icon: <ResetIcon width={18} />,
-                    onPress: () => setIsEditMode(false),
-                  }
-                : {
-                    label: "Editar",
-                    icon: <PencilIcon width={18} />,
-                    onPress: () => setIsEditMode(true),
-                  },
-              {
-                label: "Apagar",
-                icon: <TrashIcon width={18} />,
-                onPress: () =>
-                  alert(
-                    `Apagar a cola ${sheet.name}?`,
-                    "Essa ação não poderá ser desfeita",
-                    async () => {
-                      try {
-                        await deleteSheet(sheet.id);
-                        dismissAll();
-                      } catch (error) {
-                        alert(String(error));
-                      }
-                    },
-                  ),
-              },
-            ]
-              .filter(nonNullable)
-              .map(({ label, icon, onPress }) => (
-                <Pressable
-                  key={label}
-                  style={({ pressed }) => ({
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 8,
-                    paddingHorizontal: 20,
-                    paddingVertical: 8,
-                    backgroundColor: pressed ? backgroundGray : white,
-                  })}
-                  onPress={() => {
-                    onPress();
-                    setIsOptionVisible(false);
-                  }}
-                >
-                  <View>{icon}</View>
-                  <Text style={{ fontSize: 16 }}>{label}</Text>
-                </Pressable>
-              ))}
-          </View>
-        </>
-      )}
+            Copiar Link
+          </Button>
+          <Link
+            href={`https://api.whatsapp.com/send?text=${shareSheetUrl(sheet)}`}
+            target="_blank"
+          >
+            <Button>
+              <WhatsappIcon />
+            </Button>
+          </Link>
+        </View>
+      </Dialog>
     </KeyboardLayout>
   );
 };
