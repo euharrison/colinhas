394b644fb3bb7c68093ca2ce8300d485b7ebe036
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-07-21 19:35:51 -0300

    update og tags (#2)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,28 @@
 import { Stack } from "expo-router/stack";
 import { AuthProvider } from "../components/AuthProvider";
 import { LocalSettingsProvider } from "../components/LocalSettingsProvider";
+import { OgTags } from "../components/OgTags";
 import { OnboardingGate } from "../components/OnboardingGate";
 import { white } from "../theme/colors";
 
 export default function Layout() {
   return (
-    <AuthProvider>
-      <LocalSettingsProvider>
-        <OnboardingGate>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: {
-                backgroundColor: white,
-              },
-            }}
-          />
-        </OnboardingGate>
-      </LocalSettingsProvider>
-    </AuthProvider>
+    <>
+      <OgTags />
+      <AuthProvider>
+        <LocalSettingsProvider>
+          <OnboardingGate>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: {
+                  backgroundColor: white,
+                },
+              }}
+            />
+          </OnboardingGate>
+        </LocalSettingsProvider>
+      </AuthProvider>
+    </>
   );
 }
