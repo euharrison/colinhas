019bf379ed4602b37ca649cd9b4ffd1a7744277d
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-10-03 00:48:47 -0300

    render only one instrument

--- src/pages/MusicXmlPage.tsx
+++ src/pages/MusicXmlPage.tsx
@@ -1,45 +1,74 @@
 import { ScrollView, Text, View } from "react-native";
 import { Header } from "../components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   OpenSheetMusicDisplay,
   Cursor,
   VoiceEntry,
   Note,
   StemDirectionType,
+  Instrument,
 } from "opensheetmusicdisplay";
 
+/*
+TODO
+- renderizar ele totalmente na horizontal
+- renderizar elementos personalizados alinhas com as notas
+*/
+
 export const MusicXmlPage = () => {
-  const [data, setData] = useState();
+  const [instrumentIndex, setInstrumentIndex] = useState(0);
+  const [instrumentOptions, setInstrumentOptions] = useState<Instrument[]>([]);
+
+  const osmdRef = useRef<OpenSheetMusicDisplay>();
 
   useEffect(() => {
     const load = async () => {
-      // const response = await fetch("musicxml/anunciacao.xml");
-      // const response = await fetch("musicxml/anunciacao.mxl");
-      // console.log(response);
-
       const osmd = new OpenSheetMusicDisplay("osmdContainer");
       osmd.setOptions({
         backend: "svg",
-        drawTitle: true,
+        // drawTitle: true,
+        // drawPartNames: true,
         drawingParameters: "compacttight", // don't display title, composer etc., smaller margins
       });
       osmd
         // .load("http://downloads2.makemusic.com/musicxml/MozaVeilSample.xml")
         // .load("/musicxml/MozaVeilSample.xml")
-        .load("/musicxml/anunciacao.mxl")
+        // .load("/musicxml/anunciacao.mxl")
+        .load("/musicxml/Sulamericano-Marimbondo-2.0-Trombone_Cigarra.mxl")
         .then(function () {
+          console.log("on load", osmd.Sheet.Instruments);
+          osmdRef.current = osmd;
+          setInstrumentOptions(osmd.Sheet.Instruments);
+          const allParts = osmd.Sheet.Instruments;
+          allParts.forEach((part, index) => {
+            part.Visible = index === instrumentIndex;
+          });
+          const container = document.getElementById("osmdContainer");
+          if (container) {
+            container.innerHTML = "";
+          }
           osmd.render();
         });
     };
-    load();
-    // anunciacao.xml
-  }, []);
+    try {
+      load();
+    } catch (error) {
+      console.error("[ERRO]", error);
+    }
+  }, [instrumentIndex]);
 
   return (
     <View style={{ flex: 1 }}>
       <Header />
       <ScrollView>
+        <select onChange={(e) => setInstrumentIndex(Number(e.target.value))}>
+          {instrumentOptions.map((o) => (
+            <option key={o.Id} value={o.Id}>
+              {o.Id} - {o.Name}
+            </option>
+          ))}
+        </select>
         <div id="osmdContainer" />
       </ScrollView>
     </View>
