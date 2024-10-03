efe7ea1cfdbcd0e45427a0d89e4bd6c70d1ebdcf
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-10-03 00:02:12 -0300

    install opensheetmusicdisplay

--- src/pages/MusicXmlPage.tsx
+++ src/pages/MusicXmlPage.tsx
@@ -1,20 +1,47 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Header } from "../components/Header";
+import { useEffect, useState } from "react";
+import {
+  OpenSheetMusicDisplay,
+  Cursor,
+  VoiceEntry,
+  Note,
+  StemDirectionType,
+} from "opensheetmusicdisplay";
 
 export const MusicXmlPage = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const load = async () => {
+      // const response = await fetch("musicxml/anunciacao.xml");
+      // const response = await fetch("musicxml/anunciacao.mxl");
+      // console.log(response);
+
+      const osmd = new OpenSheetMusicDisplay("osmdContainer");
+      osmd.setOptions({
+        backend: "svg",
+        drawTitle: true,
+        drawingParameters: "compacttight", // don't display title, composer etc., smaller margins
+      });
+      osmd
+        // .load("http://downloads2.makemusic.com/musicxml/MozaVeilSample.xml")
+        // .load("/musicxml/MozaVeilSample.xml")
+        .load("/musicxml/anunciacao.mxl")
+        .then(function () {
+          osmd.render();
+        });
+    };
+    load();
+    // anunciacao.xml
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <Header />
-      <View
-        style={{
-          flex: 1,
-          padding: 20,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ fontSize: 20 }}>MusicXml</Text>
-      </View>
+      <ScrollView>
+        <div id="osmdContainer" />
+      </ScrollView>
     </View>
   );
 };
